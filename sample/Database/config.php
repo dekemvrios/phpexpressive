e18cc634d89dcf5d14f982f2a44b4d19b6ffa851
Rafael Becker <realfabecker@gmail.com>
2017-08-07 13:44:31 -0300

    [FIX] corrigido documentacao

--- sample/Database/config.php
+++ sample/Database/config.php
@@ -4,7 +4,7 @@ use Solis\Expressive\Classes\Illuminate\Database;
 
 Database::boot(
     [
-        'driver'   => 'pgsql',
+        'driver'   => getenv('DB_DRIVER'),
         'host'     => getenv('DB_HOST'),
         'database' => getenv('DB_NAME'),
         'username' => getenv('DB_USER'),
