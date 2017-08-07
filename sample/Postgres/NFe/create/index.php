ecebb3fa6f04a2031daa58642fb2319f5d94423e
Rafael Becker <realfabecker@gmail.com>
2017-08-07 08:00:54 -0300

    [INC] adicionado script timer para exemplo criacao nfe

--- sample/Postgres/NFe/create/index.php
+++ sample/Postgres/NFe/create/index.php
@@ -18,9 +18,11 @@ try {
         ]
     );
 
-    $last = NFe::make([
-        'iEmpCodigo' => 263
-    ])->last() or die('not found last record for NFe');
+    $last = NFe::make(
+        [
+            'iEmpCodigo' => 263,
+        ]
+    )->last() or die('not found last record for NFe');
 
     $iNFeSequencia = $last->nfesequencia + 1;
 
@@ -36,43 +38,45 @@ try {
             "iItnValorTotal" => 10,
             "iItnNcmDesc"    => "aaa",
             "iItnCstDesc"    => "bbb",
-            "iItnUnDesc"     => "ccc"
+            "iItnUnDesc"     => "ccc",
         ];
     }
 
-    $instance = NFe::make(
-        [
-            "iNFeSequencia"    => $iNFeSequencia,
-            "iEmpCodigo"       => 263,
-            "iNFeSerie"        => 3,
-            "iNFeNumero"       => 4,
-            "iFilCodigo"       => 1,
-            "sNFeModelo"       => '55',
-            "iNFeFinalidade"   => 1,
-            "iNFeSituacao"     => 1,
-            "iNFeFormaEmissao" => 1,
-            "sNFeDataEmissao"  => Date('Y-m-d'),
-            "sNFeHoraEmissao"  => Date('H:m:s'),
-            "iNFeTipoDoc"      => 1,
-            "iNFeTipoPag"      => 1,
-            "iNFeConFinal"     => 1,
-            "iNFeIndPres"      => 1,
-            "iNFeDestOp"       => 1,
-            "iNFeTipoAmb"      => 1,
-            "iNFeNatCod"       => 1,
-            "iNFeCidCod"       => 1,
-            "iNFePesCod"       => 1,
-            "iNFeValorTot"     => 1,
-            "iNFeProdValorTot" => 1,
-            "iNFeValorTotDesc" => 1,
-            "iNFeModFrete"     => 1,
-            "aProdutos"        => $produtos
-        ]
-    );
+    for ($i = $iNFeSequencia; $i <= $iNFeSequencia + 100; $i++) {
+        $instance = NFe::make(
+            [
+                "iNFeSequencia"    => $i,
+                "iEmpCodigo"       => 263,
+                "iNFeSerie"        => 3,
+                "iNFeNumero"       => 4,
+                "iFilCodigo"       => 1,
+                "sNFeModelo"       => '55',
+                "iNFeFinalidade"   => 1,
+                "iNFeSituacao"     => 1,
+                "iNFeFormaEmissao" => 1,
+                "sNFeDataEmissao"  => Date('Y-m-d'),
+                "sNFeHoraEmissao"  => Date('H:m:s'),
+                "iNFeTipoDoc"      => 1,
+                "iNFeTipoPag"      => 1,
+                "iNFeConFinal"     => 1,
+                "iNFeIndPres"      => 1,
+                "iNFeDestOp"       => 1,
+                "iNFeTipoAmb"      => 1,
+                "iNFeNatCod"       => 1,
+                "iNFeCidCod"       => 1,
+                "iNFePesCod"       => 1,
+                "iNFeValorTot"     => 1,
+                "iNFeProdValorTot" => 1,
+                "iNFeValorTotDesc" => 1,
+                "iNFeModFrete"     => 1,
+                "aProdutos"        => $produtos,
+            ]
+        );
 
-    var_dump(
-        $instance->create()
-    );
+        $instance->create();
+    }
+
+    echo 'done!';
 
 } catch (TException $exception) {
     echo $exception->toJson();
