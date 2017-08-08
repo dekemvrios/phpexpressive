e4ee25e5dfef2ca4e699d0da3cf1dce11d25f52f
Rafael Becker <realfabecker@gmail.com>
2017-08-07 21:23:58 -0300

    [UPD] atualizado script de inicializacao

--- start.sh
+++ start.sh
@@ -1,10 +1,32 @@
-# simple configuration for docker-compose
+#!/bin/sh
 
-IP=192.168.0.107 \
-INSTALL_XDEBUG=false \
-DB_DRIVER=pgsql \
-DB_HOST=192.168.0.107 \
-DB_NAME=empresarial \
-DB_USER=postgres \
-DB_PASS=4hvU1kbzGe \
+# simple start for docker-compose
+
+# ip address used to configure the debug session an the database connection
+ip_addres=$(eval hostname -I | awk '{print $1}')
+
+# you can define to not install xdebug if you want
+use_xdebug=false
+
+# database connection configuration {
+    # -- database drive
+    db_driver=??
+
+    # -- database name
+    db_name=??
+
+    # -- database user
+    db_user=??
+
+    # -- database password
+    db_pass=??
+# }
+
+IP=$ip_addres \
+INSTALL_XDEBUG=$use_xdebug \
+DB_DRIVER=$db_driver \
+DB_HOST=$ip_addres \
+DB_NAME=$db_name \
+DB_USER=$db_user \
+DB_PASS=$db_pass \
 docker-compose up -d
