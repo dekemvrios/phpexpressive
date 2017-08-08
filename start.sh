b0f2e21b1f4193e45e7b2217ca9ace98bdfb7ed7
Rafael Becker <realfabecker@gmail.com>
2017-08-07 21:32:28 -0300

    [UPD] atualizado script start.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 
-# simple start for docker-compose
+# Endereço de ip utilizado na definição da sessão do XDEBUG.
 
-# ip address used to configure the debug session an the database connection
 ip_addres=$(eval hostname -I | awk '{print $1}')
 
-# you can define to not install xdebug if you want
+# Especifica a instalação do Debugger. Deve permanecer desativado caso necessário validação de performance
+# de execução da biblioteca.
+
 use_xdebug=false
 
-# database connection configuration {
-    # -- database drive
+# Configurações da conexão com a base de dados utilizada pelo ORM.
+
+# database configuration {
+    # Driver utilizado para conexão
     db_driver=??
 
-    # -- database name
+    # Nome da base de dados
     db_name=??
 
-    # -- database user
+    # Usuário com privilégios de escrita
     db_user=??
 
-    # -- database password
+    # Senha do respectivo usuário
     db_pass=??
+
+    # Host qual contém a base de dados
+    db_host=??
 # }
+
+# Inicialização do container docker de acordo com as definições anteriores.
 
 IP=$ip_addres \
 INSTALL_XDEBUG=$use_xdebug \
 DB_DRIVER=$db_driver \
-DB_HOST=$ip_addres \
+DB_HOST=$db_host \
 DB_NAME=$db_name \
 DB_USER=$db_user \
 DB_PASS=$db_pass \
