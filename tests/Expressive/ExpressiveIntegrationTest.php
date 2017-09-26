815d19e649e764b5f6cbb9bd5e2da23e07014cc8
Rafael Becker <realfabecker@gmail.com>
2017-09-26 15:10:18 -0300

    [UPD] removed unnused use declaration in integration test

--- tests/Expressive/ExpressiveIntegrationTest.php
+++ tests/Expressive/ExpressiveIntegrationTest.php
@@ -3,7 +3,6 @@
 namespace Solis\Expressive\Test\Expressive;
 
 use PHPUnit\Framework\TestCase;
-use Solis\Breaker\Abstractions\TExceptionAbstract;
 use Solis\Expressive\Test\Fixtures\IntegrationTest\Pessoa;
 use Solis\Expressive\Test\Fixtures\IntegrationTest\DatabaseBuilder as DB;
 
