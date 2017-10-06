a1d44c55e0c43938cb2951f8bb08db5b382454a8
Rafael Becker <realfabecker@gmail.com>
2017-10-06 15:07:11 -0300

    [UPD] refactored method disposition in delete relationship builder

--- src/Classes/Illuminate/Delete/RelationshipBuilder.php
+++ src/Classes/Illuminate/Delete/RelationshipBuilder.php
@@ -5,21 +5,19 @@ namespace Solis\Expressive\Classes\Illuminate\Delete;
 use Solis\Expressive\Schema\Contracts\Entries\Property\PropertyContract;
 use Solis\Expressive\Contracts\ExpressiveContract;
 use Solis\Breaker\Abstractions\TExceptionAbstract;
-use Solis\Breaker\TException;
+use Solis\Expressive\Exception;
 
 /**
- * Class SelectBuilder
+ * Class RelationshipBuilder
  *
  * @package Solis\Expressive\Classes\Illuminate\Delete
  */
-final class RelationshipBuilder
+class RelationshipBuilder
 {
 
     /**
      * @param ExpressiveContract $model
      * @param PropertyContract   $dependency
-     *
-     * @return ExpressiveContract
      *
      * @throws TExceptionAbstract
      */
@@ -27,28 +25,91 @@ final class RelationshipBuilder
         $model,
         $dependency
     ) {
+        $dependencyValue = $this->getDependencyValue($model, $dependency);
+        if (!$dependencyValue) {
+            return;
+        }
+
+        $this->deleteHasMany($model, $dependency, $dependencyValue);
+    }
+
+    /**
+     * @param ExpressiveContract $model
+     * @param PropertyContract   $dependency
+     *
+     * @return array
+     */
+    private function getDependencyValue($model, PropertyContract $dependency): array
+    {
         $dependencyValue = $model->{$dependency->getProperty()};
 
         $dependencyValue = !is_array($dependencyValue) ? [$dependencyValue] : $dependencyValue;
 
-        $sharedFields = $dependency->getComposition()->getRelationship()->getSharedFields();
+        return $dependencyValue;
+    }
+
+    /**
+     * @param $model
+     * @param $dependency
+     * @param $dependencyValue
+     *
+     * @throws Exception
+     */
+    private function deleteHasMany($model, $dependency, $dependencyValue): void
+    {
         foreach ($dependencyValue as $item) {
-            if (!empty($sharedFields)) {
-                foreach ($sharedFields as $sharedField) {
-                    $item->{$sharedField} = $model->{$sharedField};
-                }
+
+            if ($this->hasSharedFields($dependency)) {
+                $item = $this->shareFieldsBetweenInstances($model, $dependency, $item);
             }
 
             $child = $item->search();
-            if (empty($child)) {
-                throw new TException(
-                    __CLASS__,
-                    __METHOD__,
-                    "error deleting dependency " . get_class($item) . " for " . get_class($model),
-                    500
+
+            if (!$child) {
+                throw new Exception(
+                        "error deleting dependency " . get_class($item) . " for " . get_class($model), 500
                 );
             }
+
             $child->delete();
         }
+    }
+
+    /**
+     * @param PropertyContract $dependency
+     *
+     * @return bool
+     */
+    private function hasSharedFields(PropertyContract $dependency): bool
+    {
+        return !empty($dependency->getComposition()->getRelationship()->getSharedFields());
+    }
+
+    /**
+     * @param ExpressiveContract $model
+     * @param PropertyContract   $dependency
+     * @param ExpressiveContract $instance
+     *
+     * @return ExpressiveContract
+     */
+    private function shareFieldsBetweenInstances($model, $dependency, $instance)
+    {
+        foreach ($this->getCompositionSharedFields($dependency) as $field) {
+            $instance->{$field} = $model->{$field};
+        }
+
+        return $instance;
+    }
+
+    /**
+     * @param PropertyContract $dependency
+     *
+     * @return mixed
+     */
+    private function getCompositionSharedFields(PropertyContract $dependency)
+    {
+        $sharedFields = $dependency->getComposition()->getRelationship()->getSharedFields();
+
+        return $sharedFields;
     }
 }
